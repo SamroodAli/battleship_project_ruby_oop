d77adb1e1af5c69a2e963bedb3d64134fdebbe7c
samroodAli <devsamrood@gmail.com>
2021-03-27 19:42:30 +0530

    [] and []= function for Board class to index and set position

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,21 @@
 class Board
+  attr_reader :size
+
   def initialize(num_of_rows_columns)
     @grid = Array.new(num_of_rows_columns) { Array.new(num_of_rows_columns){:N} }
     @size = num_of_rows_columns*num_of_rows_columns
   end
   
+  def [](array)
+    @grid[array[0]][array[1]]
+  end
+
+  def []=(array,value)
+    @grid[array[0]][array[1]] = value
+  end
+
+  def num_ships
+    @grid.flatten.count(:S)
+  end
+
 end
