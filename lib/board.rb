34bf47628f550f8b5e9a5c70e9282095b1644c9b
samroodAli <devsamrood@gmail.com>
2021-03-27 19:36:01 +0530

    grid and size of the grid in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
 class Board
+  def initialize(num_of_rows_columns)
+    @grid = Array.new(num_of_rows_columns) { Array.new(num_of_rows_columns){:N} }
+    @size = num_of_rows_columns*num_of_rows_columns
+  end
   
 end
