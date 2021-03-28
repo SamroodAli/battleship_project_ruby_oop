279df58163d912d6948746d390b4de17b7fcbeaf
samroodAli <devsamrood@gmail.com>
2021-03-28 14:44:06 +0530

    Player class get_move method

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,9 @@
 class Player
+  
+  def get_move
+    puts should print "enter a position with coordinates separated with a space like `4 7`"
+    input = gets.chomp
+    array = input.split.map(&:to_i)
+  end
 
 end
