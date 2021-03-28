e7188bc914dc5a2d3297c77de5a9d680e3ebc4c1
samroodAli <devsamrood@gmail.com>
2021-03-28 14:48:02 +0530

    start game method in class Battleship

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,9 +2,17 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board , :player
   def initialize(length_of_board)
     @player = Player.new
     @board = Board.new(length_of_board)
     @remaining_misses = @board.size/2
   end
+
+  def start_game
+    @board.place_random_ships
+    @board.place_random_ships
+    @board.print
+  end
+
 end
