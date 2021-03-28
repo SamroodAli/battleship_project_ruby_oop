f017c99f76973cc9ef321c21ccbd0d4125dedd80
samroodAli <devsamrood@gmail.com>
2021-03-28 14:47:01 +0530

    constructor method initialize in class Battleship

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,5 +2,9 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+  def initialize(length_of_board)
+    @player = Player.new
+    @board = Board.new(length_of_board)
+    @remaining_misses = @board.size/2
+  end
 end
